b6162fd424bd6f4a47f746f59f3e1f28e3889222
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-04 19:50:39 -0400

    added complete script

--- script.js
+++ script.js
@@ -40,7 +40,51 @@ const randomFunc ={
 
  //function to generate the password:
  function generatePassword(lower, upper, number, symbol, length){
-    let generatePassword = '';
+    let generatedPassword = '';
     const typesCount = lower + upper + number + symbol;
     const typesArray = [{lower}, {upper}, {number}, {symbol}].filter(item=>Object.values(item)[0]);
+
+    //verify if a least ones of the checkbox is checked:
+        if(typesCount===0){
+            return '';
+        }
+
+        for(let index = 0; index < length; index+=typesCount){
+            typesArray.forEach(type =>{
+                const funcName = Object.keys(type)[0]
+                generatedPassword +=randomFunc[funcName]()
+            })
+        }
+        const finalPassword = generatedPassword.slice(0, length)
+        return finalPassword;
  }
+
+ function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
+ }
+
+ function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+ }
+
+ function getRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+ }
+
+ function getRandomSymbol(){
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+ }
+
+
+
+
+
+
+
+
+
+
+
+
